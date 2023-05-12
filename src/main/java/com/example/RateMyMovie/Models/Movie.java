756c11c7c5a378841c2a2d8f089060962730606b
Pragati Srivastava <102436055+prag-sri@users.noreply.github.com>
2023-05-12 14:24:28 +0530

    Update Movie.java

--- src/main/java/com/example/RateMyMovie/Models/Movie.java
+++ src/main/java/com/example/RateMyMovie/Models/Movie.java
@@ -17,7 +17,7 @@ public class Movie {
     @Id
     private String tconst;
     private String titleType;
-    @Column(name="primaryTitle", unique = true)
+    @Column(name="primaryTitle")
     private String primaryTitle;
     @Column(name="runtimeMinutes")
     private int runtimeMinutes;
